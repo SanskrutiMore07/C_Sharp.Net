76721b18d639e545c0feeab812038a0190e1a249
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-10-10 20:20:43 +0530

    Consol Program

---
+++ consol_programs/Addition_Of_Two_Numbers/Addition_Of_Two_Numbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Addition_Of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, sum;
+            Console.WriteLine("Enter The Vlues Of a  : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter The Vlues Of b  : ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+            sum = a + b;
+            Console.WriteLine("{0}+{1}={2}",a,b,sum);
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
