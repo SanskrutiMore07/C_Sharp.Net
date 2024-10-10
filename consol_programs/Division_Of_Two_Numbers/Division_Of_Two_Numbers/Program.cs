b29dac37ae7979a44879e803bac40f03a098c697
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-10-10 20:21:32 +0530

    Consol Program

---
+++ consol_programs/Division_Of_Two_Numbers/Division_Of_Two_Numbers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division_Of_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num1, Num2, Result;
+            Console.WriteLine("Enter First Number:");
+            Num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Second Number:");
+            Num2 = Convert.ToInt32(Console.ReadLine());
+
+            Result = Num1/Num2;
+            Console.WriteLine("{0}/{1}={2}", Num1, Num2, Result);
+
+            Console.WriteLine("Press Any Key To Exit");
+            Console.ReadKey();
+        }
+    }
+}
