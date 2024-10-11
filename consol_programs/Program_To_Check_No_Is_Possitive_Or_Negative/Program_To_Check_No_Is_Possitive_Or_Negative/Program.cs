d96c57516617168680146122fc367db6c42e9c1a
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-10-11 20:58:02 +0530

    Consol Program

---
+++ consol_programs/Program_To_Check_No_Is_Possitive_Or_Negative/Program_To_Check_No_Is_Possitive_Or_Negative/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_To_Check_No_Is_Possitive_Or_Negative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No = 0;
+            Console.WriteLine("Enter any number");
+            No = Convert.ToInt32(Console.ReadLine());
+            if(No>=0)
+            {
+                Console.WriteLine("Given number is possitive");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Given number is negative");
+                Console.ReadLine();
+            }
+        }
+    }
+}
