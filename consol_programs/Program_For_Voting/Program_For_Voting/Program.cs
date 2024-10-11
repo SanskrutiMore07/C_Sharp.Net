d84e4c51af7f81c66d266b8beb9b44682ad7a006
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-10-11 20:55:59 +0530

    Consol Program

---
+++ consol_programs/Program_For_Voting/Program_For_Voting/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program_For_Voting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int no;
+            Console.WriteLine("Enter Age of member :");
+            no = Convert.ToInt32(Console.ReadLine());
+            if(no>=18)
+            {
+                Console.WriteLine("Is eligible for voting");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Not eiligible for voting ");
+                Console.ReadLine();
+            }
+                 
+        }
+    }
+}
