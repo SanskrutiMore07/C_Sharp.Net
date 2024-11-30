80c8dcac3fce39d93182cf4061248990a67a2c2f
SanskrutiMore07 <sanskrutim2004@gmail.com>
2024-11-30 21:46:49 +0530

    While Loop

---
+++ consol_programs/While_Loop/While_Loop/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i=1;
+            
+            while (i<=4)
+            {
+                Console.WriteLine("Sanskruti",i);
+                Console.ReadLine();
+                i++;
+            }
+        }
+    }
+}
