6c5fdd134ba25a409d64fa1fe0b31301a1e81790
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-04-02 21:55:31 +0530

    Consol Program

---
+++ consol_programs/Find_Length_Of_Array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Find_Length_Of_Array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 1, 2, 3, 4, 5, 6, 7};
+
+            int length = numbers.Length;
+            Console.WriteLine("The Length og array is :" + length);
+            Console.ReadLine();
+        }
+
+
+    }
+}
