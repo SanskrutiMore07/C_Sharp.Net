682efc281b75d9180354264aa8fa18426989474a
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-04-02 21:47:41 +0530

    Consol Program

---
+++ consol_programs/Program_To_Find_Factorial_Of_Given_Number/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Remoting.Services;
+
+namespace Program_To_Find_Factorial_Of_Given_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i, fact = 1, number;
+            Console.WriteLine("Enter any Number :");
+            number = Convert.ToInt32(Console.ReadLine());
+            for(i = 1;i<=number;i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial Of " +  number  + " is: " + fact);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ consol_programs/Program_To_Find_Factorial_Of_Given_Number/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Program_To_Find_Factorial_Of_Given_Number")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Program_To_Find_Factorial_Of_Given_Number")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c87f1d0-a77a-47e5-909f-048541746e9b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
