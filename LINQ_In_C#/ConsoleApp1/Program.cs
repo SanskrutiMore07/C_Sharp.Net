17cb88ddddabb7083bad4a5f001254b0abf315e7
SanskrutiMore07 <sanskrutim2004@gmail.com>
2025-06-13 19:51:30 +0530

    linq

---
+++ LINQ_In_C#/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Age = { 12, 23, 45, 67, 80, 54, 14, 17, 20 };
+            var a = from i in Age where i >= 20 select i;
+
+            foreach(int item in a)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadLine();
+        }
+    }
+}
