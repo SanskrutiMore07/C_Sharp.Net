1d6c8aff3f4e24b2560db2b3a6a1fe9a7517b2fc
SanskrutiMore07 <sanskrutim2004@gmail.com>
2026-01-21 21:33:00 +0530

    Program

---
+++ consol_programs/Palindrom_Number/Palindrom_Number/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Palindrom_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No = 121, Temp = No , Rev = 0;
+
+            while (No > 0)
+            {
+                Rev = Rev * 10 + No % 10;
+                No /= 10;
+            }
+            if(Temp == Rev)
+            {
+                Console.WriteLine("Given Number Is palindrome");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Given Number Is Not palindrome");
+                Console.ReadLine();
+            }
+        }
+    }
+}
