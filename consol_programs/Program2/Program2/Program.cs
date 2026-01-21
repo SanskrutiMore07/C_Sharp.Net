8fd180a4cccbd5cf0b8431c5bfd71538ade05555
SanskrutiMore07 <sanskrutim2004@gmail.com>
2026-01-21 21:33:45 +0530

    Program

---
+++ consol_programs/Program2/Program2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter Your Name : ");
+            string Name = Console.ReadLine();
+
+            Console.Write("Enter Age : ");
+            int Age = Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine($"Hello {Name}! Your Age Is {Age}");
+            Console.ReadLine();
+        }
+    }
+}
