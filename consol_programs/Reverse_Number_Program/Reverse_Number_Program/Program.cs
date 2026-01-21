c4a36e4501f2130bdc247008bc5c302125023de6
SanskrutiMore07 <sanskrutim2004@gmail.com>
2026-01-21 21:34:19 +0530

    Program

---
+++ consol_programs/Reverse_Number_Program/Reverse_Number_Program/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reverse_Number_Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No = 12345, rev = 0;
+
+            while(No>0)
+            {
+                rev = rev * 10 + No % 10;
+                No /= 10;
+            }
+
+            Console.WriteLine("Revers No :" + rev);
+            Console.ReadLine();
+        }
+    }
+}
