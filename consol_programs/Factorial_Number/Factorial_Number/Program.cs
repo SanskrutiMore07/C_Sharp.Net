4bd473d5fff5bd02a74051c36f54b12c9a31c9f2
SanskrutiMore07 <sanskrutim2004@gmail.com>
2026-01-23 20:58:04 +0530

    Program

---
+++ consol_programs/Factorial_Number/Factorial_Number/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No = 5;
+            int Fact = 1;
+
+            for(int i = 1; i<=No;i++)
+            {
+                Fact = Fact * i;
+            }
+            Console.WriteLine("Factorial Of Given Number "+ No +" is "+ Fact);
+            Console.ReadLine();
+        }
+    }
+}
