0e6dd56edb1d59cc1e26677c34d3f6af366e3ec8
SanskrutiMore07 <sanskrutim2004@gmail.com>
2026-01-23 20:58:30 +0530

    Program

---
+++ consol_programs/Fibonacci_Series/Fibonacci_Series/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0, b = 1, c;
+            Console.WriteLine("Fibonacci Series");
+
+            for(int i = 1; i<=10;i++)
+            {
+                Console.Write(a + "");
+                Console.ReadLine();
+                c = a + b;
+                a = b;
+                b = c;
+            }
+        }
+    }
+}
