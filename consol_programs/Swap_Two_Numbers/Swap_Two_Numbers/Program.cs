fd9d9ac274227625642101cd04bac70ac1966ee3
SanskrutiMore07 <sanskrutim2004@gmail.com>
2026-01-25 20:09:09 +0530

    Program

---
+++ consol_programs/Swap_Two_Numbers/Swap_Two_Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Swap_Two_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = 10, B = 20;
+
+            A = A + B;
+            B = A - B;
+            A = A - B;
+
+            Console.WriteLine("A = " + A);
+            Console.ReadLine();
+            Console.WriteLine("B = " + B);
+            Console.ReadLine();
+
+        }
+    }
+}
